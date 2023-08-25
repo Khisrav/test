a115c17f983406b6a15f607178b260049a2135db
= Khisrav <= kh.khisrav2018@gmail.com>
2023-08-25 07:18:36 +0300

    mobile menu fixed

--- scripts.js
+++ scripts.js
@@ -1,8 +1,3 @@
-document.querySelector(".hamburger").addEventListener("click", function () {
-    document.querySelector(".header").classList.toggle("mobile");
-    document.querySelector("body").classList.toggle("hidden");
-});
-
 const parentDiv = document.querySelector('.big-text');
 const childDiv = document.querySelector('.hero-img');
 const smoother = document.querySelector('.smoother');
@@ -11,3 +6,41 @@ function setParentHeightToChild() {
     smoother.style.height = parentDiv.clientHeight + 'px';
 }
 setParentHeightToChild();
+
+const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("burger");
+const headerMenu = document.getElementById("header");
+
+// Open Close Navbar Menu on Click Burger
+if (burgerMenu && navbarMenu) {
+   burgerMenu.addEventListener("click", () => {
+      burgerMenu.classList.toggle("is-active");
+      navbarMenu.classList.toggle("is-active");
+   });
+}
+
+// Close Navbar Menu on Click Menu Links
+document.querySelectorAll(".menu-link").forEach((link) => {
+   link.addEventListener("click", () => {
+      burgerMenu.classList.remove("is-active");
+      navbarMenu.classList.remove("is-active");
+   });
+});
+
+// Change Header Background on Scrolling
+window.addEventListener("scroll", () => {
+   if (this.scrollY >= 85) {
+      headerMenu.classList.add("on-scroll");
+   } else {
+      headerMenu.classList.remove("on-scroll");
+   }
+});
+
+// Fixed Navbar Menu on Window Resize
+window.addEventListener("resize", () => {
+   if (window.innerWidth > 768) {
+      if (navbarMenu.classList.contains("is-active")) {
+         navbarMenu.classList.remove("is-active");
+      }
+   }
+});
